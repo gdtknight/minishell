9b51e2e2cbb82ed1a3cdac9b575346255d00bb3c
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-01 17:39:38 +0900

    fix: envp.h - add missing header "hashmap.h"

--- includes/envp.h
+++ includes/envp.h
@@ -6,12 +6,14 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 16:47:24 by yoshin            #+#    #+#             */
-/*   Updated: 2025/07/31 16:52:52 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/01 17:38:07 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENVP_H
 # define ENVP_H
+
+# include "hashmap.h"
 
 /* --- envp_utils.c --- */
 
