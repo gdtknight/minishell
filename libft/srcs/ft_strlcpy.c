974d25ef222ae59c9b74ce61a5b365acdfa59217
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-02 14:42:23 +0900

    fix: handle null src in ft_strlcpy to prevent segmentation fault

--- libft/srcs/ft_strlcpy.c
+++ libft/srcs/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 15:01:44 by yoshin            #+#    #+#             */
-/*   Updated: 2025/01/20 21:22:53 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/02 14:38:22 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	srclen = ft_strlen(src);
 	if (srclen + 1 < size)
 	{
-		while (*src)
+		while (src && *src)
 			*dst++ = *src++;
 		*dst = '\0';
 		return (srclen);
