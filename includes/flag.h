a5766a946e6f1563090f1df81c40f5043b2deac4
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-03 17:06:25 +0900

    feat: flag.h

---
+++ includes/flag.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flag.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 02:02:31 by yoshin            #+#    #+#             */
+/*   Updated: 2025/08/02 02:04:31 by yoshin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FLAG_H
+# define FLAG_H
+
+/*
+ * Quoatation Mark 관련 플래그
+*/
+
+# define C_SQUOTE			(1 << 0)
+# define C_DQUOTE			(1 << 1)
+# define C_BACKSLASH		(1 << 2)
+# define C_ENVP				(1 << 3)
+# define C_TILDE			(1 << 4)
+
+#endif
