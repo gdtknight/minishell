3d5ac681ba54bef5826649df9c5704df3cc38907
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-03 21:40:19 +0900

    test: add dot_test case in expand_envp_test.c

--- tests/expand/expand_envp_test.c
+++ tests/expand/expand_envp_test.c
@@ -6,7 +6,7 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 21:24:49 by yoshin            #+#    #+#             */
-/*   Updated: 2025/08/03 19:03:00 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/03 20:17:36 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,26 @@
 #include "shell_data.h"
 #include "tokenizer.h"
 
+void	expand_envp_dot_test(void)
+{
+	static const char	*dot_str = "$a2.txt";
+	t_token		*test_token;
+
+	test_token = create_empty_token();
+	test_token->type = TK_WORD;
+	test_token->value = ft_strdup(dot_str);
+	test_token = expand_envp(test_token);
+	if (ft_strlen(test_token->value) == ft_strlen(".txt")
+		&& (ft_strncmp(test_token->value, ".txt", ft_strlen(test_token->value)) == 0))
+		printf("[SUCCESS] before value : \"%s\", after value : \"%s\"\n",
+			dot_str, (char *)(test_token->value));
+	else
+		printf("[FAIL] before value : \"%s\", after value : \"%s\"\n",
+			dot_str, (char *)(test_token->value));
+	free(test_token->value);
+	free(test_token);
+}
+
 void	expand_envp_fail1(void)
 {
 	const char	*path_var = ft_strjoin("SHLVL:",getenv("SHLVL"));
@@ -29,7 +49,6 @@ void	expand_envp_fail1(void)
 	token->type = TK_WORD;
 	token->value = ft_strdup("SHLVL:\\$SHLVL");
 	token = expand_envp(token);
-	printf("\n");
 	if (ft_strncmp(token->value, path_var, ft_strlen(token->value)) != 0)
 		printf("[SUCCESS] before value : \"%s\", after value : \"%s\"\n",
 			"SHLVL:\\$SHLVL", (char *)(token->value));
@@ -87,6 +106,8 @@ int	main(int argc, char *argv[], char *envp[])
 	expand_envp_success2();
 	printf("\n\n");
 	expand_envp_fail1();
+	printf("\n\n");
+	expand_envp_dot_test();
 	clear_hashmap(&(get_shell_data()->envp_map));
 	exit(EXIT_SUCCESS);
 }
