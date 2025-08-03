91d9e6428ffcc6ac6d9f74f583d2675395df14ef
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-03 18:45:05 +0900

    fix: ignore delimiter following escape character
    - fix an issue where delimiters were not ignored when preceded by an
    escape character (\)

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 20:56:21 by yoshin            #+#    #+#             */
-/*   Updated: 2025/08/02 14:22:59 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/03 17:55:51 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@
 
 /* --- find_next_delim.c --- */
 
-char	*find_next_delim(char *str, t_boolean (*predicate)(char), char mask);
+char	*find_next_delim(
+			char *str,
+			t_boolean (*predicate)(char),
+			unsigned char mask);
 
 /* --- heredoc.c --- */
 
--- srcs/utils/find_next_delim.c
+++ srcs/utils/find_next_delim.c
@@ -6,7 +6,7 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 02:00:47 by yoshin            #+#    #+#             */
-/*   Updated: 2025/08/02 03:56:40 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/03 18:40:50 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,39 @@
 
 #include "def.h"
 
-static void	update_flag(char c, char *flag, char mask);
+static t_boolean	update_flag(char c, char *flag, char mask);
 
-char	*find_next_delim(char *str, t_boolean (*predicate)(char), char mask)
+char	*find_next_delim(char *str, t_boolean (*predicate)(char), unsigned char mask)
 {
 	char	flag;
 
 	flag = 0;
 	while (*str)
 	{
-		update_flag(*str, &flag, mask);
-		if (!(flag & mask) && predicate(*str))
+		if (update_flag(*str, &flag, mask))
+			str++;
+		if ((flag & mask) == 0 && predicate(*str))
 			return (str);
 		str++;
 	}
 	return (str);
 }
 
-static void	update_flag(char c, char *flag, char mask)
+static t_boolean	update_flag(char c, char *flag, char mask)
 {
+	char	old_flag;
+
+	old_flag = *flag;
 	if (!(*flag & C_BACKSLASH))
 	{
 		if ((mask & C_BACKSLASH) && c == '\\')
-			*flag ^= C_BACKSLASH;
+			*flag |= C_BACKSLASH;
 		else if ((mask & C_SQUOTE) && !(*flag & C_DQUOTE) && c == '\'')
 			*flag ^= C_SQUOTE;
 		else if ((mask & C_DQUOTE) && !(*flag & C_SQUOTE) && c == '"')
 			*flag ^= C_DQUOTE;
 	}
 	else
-		*flag ^= C_BACKSLASH;
+		*flag &= ~C_BACKSLASH;
+	return (*flag != old_flag);
 }
