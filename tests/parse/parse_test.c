eb3853ba750493c12f5b856a81dfc8910689a22c
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-04 19:30:34 +0900

    test: update test_input case in parse_test.c

--- tests/parse/parse_test.c
+++ tests/parse/parse_test.c
@@ -6,7 +6,7 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 10:41:00 by yoshin            #+#    #+#             */
-/*   Updated: 2025/07/30 21:39:30 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/04 02:06:00 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,8 @@ int	main(void)
 static t_token	*create_test_tk_lst(void)
 {
 	static char	*test_input = \
-		"< infile.txt cmd1 -n option1 | cmd2 infile2.txt "
-		"&& cmd3 -option3 | cmd4 -option4 | (cmd5 -option5 arg5 && cmd6 -option6 >> append6.txt)";
+		"< infile.txt cmd1 -n option1 | cmd2 < infile2-1.txt < infile2-2.txt -option2-1 arg2-1 arg2-2"
+		"&& cmd3 -option3 | cmd4 -option4-1 -option4-2 arg4-1 arg4-2 | (cmd5 -option5 arg5 && cmd6 -option6 >> append6.txt)";
 	t_token		*test_lst;
 
 	test_lst = tokenize_input(test_input);
