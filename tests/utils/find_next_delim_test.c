3b5657e62c45493325a88226cad448085f2ace2f
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-05 21:26:44 +0900

    chore: fix norminette issues and remove duplicate test in find_next_delim_test.c
    - Resolve style violations flagged by norminette
    - Remove redundant test case

--- tests/utils/find_next_delim_test.c
+++ tests/utils/find_next_delim_test.c
@@ -6,7 +6,7 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 18:47:24 by yoshin            #+#    #+#             */
-/*   Updated: 2025/08/05 16:40:41 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/05 21:23:09 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,9 @@
 #include "flag.h"
 #include "utils.h"
 
-static t_boolean	pred_backslash(char c);
 static t_boolean	pred_dollar(char c);
-static t_boolean	pred_dquote(char c);
-static t_boolean	pred_squote(char c);
 
-void	find_next_delim_test1(void)
+void	find_next_delim_test(void)
 {
 	static char	*test_str = "\"abc $ def\" \\$ 'ghi $ jkl' mno $ pqr";
 	char		*cursor[4];
@@ -51,107 +48,13 @@ void	find_next_delim_test1(void)
 	printf("cursor4 (C_BACKSLASH | C_DQUOTE | C_SQUOTE) : %s\n", cursor[3]);
 }
 
-void	find_next_delim_test2(void)
-{
-	static char	*test_str = "\"abc $\"\\$ \'abc $ def\'";
-	char		*cursor[3];
-
-	cursor[0] = find_next_delim(
-			test_str,
-			pred_dquote,
-			C_BACKSLASH);
-	cursor[1] = find_next_delim(
-			test_str,
-			pred_dquote,
-			C_BACKSLASH | C_DQUOTE);
-	cursor[2] = find_next_delim(
-			test_str,
-			pred_dquote,
-			C_BACKSLASH | C_DQUOTE | C_SQUOTE);
-	printf("set predicate to pred_dquote\n");
-	printf("test_str : %s\n", test_str);
-	printf("cursor1  : %s\n", cursor[0]);
-	printf("cursor2  : %s\n", cursor[1]);
-	printf("cursor3  : %s\n", cursor[2]);
-}
-
-void	find_next_delim_test3(void)
-{
-	static char	*test_str = "\"abc $\"\\$ \'abc $ def\'";
-	char		*cursor[3];
-
-	cursor[0] = find_next_delim(
-			test_str,
-			pred_backslash,
-			C_BACKSLASH);
-	cursor[1] = find_next_delim(
-			test_str,
-			pred_backslash,
-			C_BACKSLASH | C_DQUOTE);
-	cursor[2] = find_next_delim(
-			test_str,
-			pred_backslash,
-			C_BACKSLASH | C_DQUOTE | C_SQUOTE);
-	printf("set predicate to pred_backslash\n");
-	printf("test_str : %s\n", test_str);
-	printf("cursor1  : %s\n", cursor[0]);
-	printf("cursor2  : %s\n", cursor[1]);
-	printf("cursor3  : %s\n", cursor[2]);
-}
-
-void	find_next_delim_test4(void)
-{
-	static char	*test_str = "\"abc $\"\\$ \'abc $ def\'";
-	char		*cursor[3];
-
-	cursor[0] = find_next_delim(
-			test_str,
-			pred_squote,
-			C_BACKSLASH);
-	cursor[1] = find_next_delim(
-			test_str,
-			pred_squote,
-			C_BACKSLASH | C_DQUOTE);
-	cursor[2] = find_next_delim(
-			test_str,
-			pred_squote,
-			C_BACKSLASH | C_DQUOTE | C_SQUOTE);
-	printf("set predicate to pred_squote\n");
-	printf("test_str : %s\n", test_str);
-	printf("cursor1  : %s\n", cursor[0]);
-	printf("cursor2  : %s\n", cursor[1]);
-	printf("cursor3  : %s\n", cursor[2]);
-}
-
 int	main(void)
 {
-	printf("\n\n----- find_next_delim_test1 -----\n");
-	find_next_delim_test1();
-	printf("\n\n----- find_next_delim_test2 -----\n");
-	find_next_delim_test2();
-	printf("\n\n----- find_next_delim_test3 -----\n");
-	find_next_delim_test3();
-	printf("\n\n----- find_next_delim_test4 -----\n");
-	find_next_delim_test4();
+	find_next_delim_test();
 	exit(EXIT_SUCCESS);
-}
-
-static t_boolean	pred_backslash(char c)
-{
-	return (c == '\\');
 }
 
 static t_boolean	pred_dollar(char c)
 {
 	return (c == '$');
-}
-
-static t_boolean	pred_dquote(char c)
-{
-	return (c == '\"');
-}
-
-static t_boolean	pred_squote(char c)
-{
-	return (c == '\'');
 }
