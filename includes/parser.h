ea0d762467f74c882a85600f218683bb525acaec
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-15 21:05:42 +0900

    chore: clean up unnecessary comments

--- includes/parser.h
+++ includes/parser.h
@@ -6,77 +6,9 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 12:19:50 by yoshin            #+#    #+#             */
-/*   Updated: 2025/07/30 21:40:09 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/15 21:04:33 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
- * <shell_input>    ::= <list> <newline_list>
- *
- * <list>           ::= <and_or>
- *                    | <list> ";" <and_or>
- *                    | <list> "&" <and_or>
- *
- * <and_or>         ::= <pipeline>
- *                    | <and_or> "&&" <pipeline>
- *                    | <and_or> "||" <pipeline>
- * 
- * <pipeline>       ::= <command>
- *                    | <pipeline> "|" <command>
- *                    | <pipeline> "|&" <command>
- * 
- * <command>        ::= <simple_command>
- *                    | <compound_command>
- *                    | <function_def>
- * 
- * <simple_command> ::= <cmd_prefix> <cmd_word> <cmd_suffix>
- *                    | <cmd_word> <cmd_suffix>
- *                    | <cmd_prefix> <cmd_word>
- *                    | <cmd_word>
- * 
- * <cmd_prefix>     ::= <io_redirect>
- *                    | <cmd_prefix> <io_redirect>
- *                    | <assignment_word>
- *                    | <cmd_prefix> <assignment_word>
- * 
- * <cmd_word>       ::= <word>
- * 
- * <cmd_suffix>     ::= <io_redirect>
- *                    | <cmd_suffix> <io_redirect>
- *                    | <word>
- *                    | <cmd_suffix> <word>
- * 
- * <io_redirect>    ::= [<number>] ">" <word>
- *                    | [<number>] ">>" <word>
- *                    | [<number>] "<" <word>
- *                    | [<number>] "<<" <word>
- *                    | [<number>] "<>" <word>
- * 
- * <assignment_word>::= <name> "=" <word>
- *
- * <word_list>         ::= <word>
- *                      | <word_list> <word>
- *
- * <name>              ::= <letter> { <letter> | <digit> | "_" }
- *
- * <pattern>           ::= <word>
- * 
- * <word>              ::= <quoted_word> | <unquoted_word>
- * 
- * <quoted_word>       ::= "'" <unquoted_word> "'" | '"' <unquoted_word> '"'
- * <unquoted_word>     ::= { <char> }
- * <char>              ::= (any printable character
- *                          except whitespace and reserved chars)
- * 
- * <number>            ::= <digit> | <number> <digit>
- * <digit>             ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
- * 
- * <letter>            ::= "a" | ... | "z" | "A" | ... | "Z"
- * 
- * <newline>           ::= "\n"
- * <newline_list>      ::= <newline>
- *                      | <newline_list> <newline>
- */
 
 #ifndef PARSER_H
 # define PARSER_H
