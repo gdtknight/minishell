587be544965f71b26352046637f116428d0810c4
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-15 20:47:26 +0900

    feat: implement environment variable expansion

--- srcs/expand/expand_envp.c
+++ srcs/expand/expand_envp.c
@@ -5,96 +5,138 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/02 01:00:10 by yoshin            #+#    #+#             */
-/*   Updated: 2025/08/05 21:28:04 by yoshin           ###   ########.fr       */
+/*   Created: 2025/08/13 22:52:07 by yoshin            #+#    #+#             */
+/*   Updated: 2025/08/15 20:39:34 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-
-#include "libft.h"
+#include <unistd.h>
 
 #include "def.h"
-#include "utils.h"
-#include "hashmap.h"
-#include "shell_data.h"
 
 #include "flag.h"
+#include "hashmap.h"
+#include "libft.h"
+#include "shell_data.h"
+#include "utils.h"
 
-#include "tokenizer.h"
-#include "expand.h"
+static char			*replace_param(char *str, char **new_value);
+static char			*replace_env_key(char *str, char **new_value);
+static char			*replace_single_char_param(char *str, char **new_value);
+static t_boolean	is_dollar_sign(char c);
 
-static void			replace_envp(
-						char **value,
-						const char *cur,
-						const char *delim_pos);
-static char			*extract_envp(const char *delim_pos);
-static t_boolean	is_expansion(char c);
-
-t_token	*expand_envp(t_token *token)
+char	*expand_envp(char *value)
 {
-	char		*cur;
-	char		*delim_pos;
-	char		*new_value;
-	char		*envp;
-
-	new_value = NULL;
-	cur = token->value;
-	delim_pos = find_next_delim(cur, is_expansion, C_SQUOTE | C_BACKSLASH);
-	while (*cur && *delim_pos)
-	{
-		envp = extract_envp(delim_pos);
-		replace_envp(&new_value, cur, delim_pos);
-		cur = delim_pos + ft_strlen(envp) + 1;
-		delim_pos = find_next_delim(cur, is_expansion, C_SQUOTE | C_BACKSLASH);
-		free(envp);
-	}
-	if (new_value)
-	{
-		free(token->value);
-		token->value = new_value;
-	}
-	return (token);
-}
-
-static void	replace_envp(char **value, const char *cur, const char *delim_pos)
-{
+	char	*new_value;
 	char	*temp;
 	char	*prefix;
-	char	*envp;
-	char	*envp_value;
+	char	*cur;
 
-	temp = (*value);
-	prefix = ft_substr(cur, 0, (delim_pos - cur));
-	envp = extract_envp(delim_pos);
-	if (ft_strncmp(envp, "0", ft_strlen(envp)) == 0)
-		envp_value = ft_strdup("minishell");
-	else
+	new_value = NULL;
+	cur = find_next_delim(value, is_dollar_sign, C_BACKSLASH | C_SQUOTE);
+	while (*cur)
 	{
-		envp_value = ft_strdup(get_value(&(get_shell_data())->envp_map, envp));
-		if (!envp_value)
-			envp_value = ft_strdup("");
+		temp = new_value;
+		prefix = ft_substr(value, 0, cur - value);
+		new_value = ft_strjoin(temp, prefix);
+		free(temp);
+		free(prefix);
+		value = replace_param(cur, &new_value);
+		cur = find_next_delim(value, is_dollar_sign, C_BACKSLASH | C_SQUOTE);
 	}
-	*value = ft_multiplejoin(temp, prefix, envp_value);
-	free(envp);
+	temp = new_value;
+	prefix = ft_substr(value, 0, cur - value);
+	new_value = ft_strjoin(temp, prefix);
 	free(temp);
 	free(prefix);
-	free(envp_value);
+	return (new_value);
 }
 
-static char	*extract_envp(const char	*delim_pos)
+static char	*replace_param(char *str, char **new_value)
 {
-	char	*end_pos;
+	int		num;
+	char	*temp;
+	char	*cur;
+	char	*env_value;
 
-	if (*(delim_pos + 1) == '0')
-		return (ft_strdup("0"));
-	end_pos = (char *)delim_pos + 1;
-	while (ft_isalnum(*end_pos) || *end_pos == '_')
-		end_pos++;
-	return (ft_substr(delim_pos, 1, end_pos - delim_pos));
+	cur = str + 1;
+	if (ft_isdigit(*cur))
+	{
+		num = ft_atoi(cur);
+		while (ft_isdigit(*(++cur)))
+			;
+		if (num == 0)
+			env_value = ft_strdup("minishell");
+		else
+			env_value = ft_strdup("");
+		temp = *new_value;
+		(*new_value) = ft_strjoin(temp, env_value);
+		free(temp);
+		free(env_value);
+		return (cur);
+	}
+	else if (*cur == '*' || *cur == '@' || *cur == '?'
+		|| *cur == '$' || *cur == '!' || *cur == '_')
+		return (replace_single_char_param(cur, new_value));
+	else if (ft_isalpha(*cur) || *cur == '_')
+		return (replace_env_key(cur, new_value));
+	return (str);
 }
 
-static t_boolean	is_expansion(char c)
+/*
+ *    `$*` : 모든 위치 매개변수(하나의 문자열로)
+ *    `$@` : 모든 위치 매개변수(각각 따로, `" "`로 감쌌을 때 차이)
+ *    `$?` : 직전 명령의 종료 상태값(종료 코드)
+ *    `$$` : 현재 쉘 프로세스의 PID
+ *    `$!` : 마지막 백그라운드 실행 프로세스의 PID
+ *    `$_` : 이전 명령의 마지막 인자
+ */
+static char	*replace_single_char_param(char *cur, char **new_value)
+{
+	char	*value;
+	char	*temp;
+
+	if (*cur == '*' || *cur == '@' || *cur == '_')
+		value = ft_strdup("");
+	else if (*cur == '?')
+		value = ft_itoa(get_shell_data()->last_status);
+	else if (*cur == '$' || *cur == '!')
+		value = ft_itoa(getpid());
+	else
+		value = NULL;
+	temp = *new_value;
+	*new_value = ft_strjoin(temp, value);
+	free(temp);
+	free(value);
+	return (++cur);
+}
+
+static char	*replace_env_key(char *str, char **new_value)
+{
+	char	*cur;
+	char	*key;
+	char	*value;
+	char	*temp;
+
+	cur = str;
+	while (ft_isalnum(*cur) || *cur == '_')
+		cur++;
+	key = ft_substr(str, 0, cur - str);
+	value = get_value(&((get_shell_data())->envp_map), key);
+	if (value)
+		value = ft_strdup(value);
+	else
+		value = ft_strdup("");
+	temp = *new_value;
+	*new_value = ft_strjoin(temp, value);
+	free(key);
+	free(temp);
+	free(value);
+	return (cur);
+}
+
+static t_boolean	is_dollar_sign(char c)
 {
 	return (c == '$');
 }
--- tests/expand/expand_envp_test.c
+++ tests/expand/expand_envp_test.c
@@ -1,100 +1,61 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   expand_envp_test.c                                 :+:      :+:    :+:   */
+/*   expand_env_param_test.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/02 21:24:49 by yoshin            #+#    #+#             */
-/*   Updated: 2025/08/05 16:31:17 by yoshin           ###   ########.fr       */
+/*   Created: 2025/08/14 02:10:10 by yoshin            #+#    #+#             */
+/*   Updated: 2025/08/15 02:58:40 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
-
-#include "hashmap.h"
-#include "libft.h"
 
 #include "expand.h"
+#include "debug.h"
 #include "shell_data.h"
-#include "tokenizer.h"
 
-void	expand_envp_dot_test(void)
+void	expand_envp_success_test1(void)
 {
-	static const char	*dot_str = "$a2.txt";
-	t_token				*test_token;
+	char	*test_str1 = "$0";
+	char	*test_str2 = "$?";
+	char	*test_str3 = "abc$0def";
+	char	*test_str4 = "abc$?def";
 
-	test_token = create_empty_token();
-	test_token->type = TK_WORD;
-	test_token->value = ft_strdup(dot_str);
-	test_token = expand_envp(test_token);
-	if (ft_strlen(test_token->value) == ft_strlen(".txt")
-		&& (ft_strncmp(test_token->value, \
-				".txt", ft_strlen(test_token->value)) == 0))
-		printf("[SUCCESS] before value : \"%s\", after value : \"%s\"\n",
-			dot_str, (char *)(test_token->value));
-	else
-		printf("[FAIL] before value : \"%s\", after value : \"%s\"\n",
-			dot_str, (char *)(test_token->value));
-	free(test_token->value);
-	free(test_token);
+	char	*expanded_value1 = expand_envp(test_str1);
+	char	*expanded_value2 = expand_envp(test_str2);
+	char	*expanded_value3 = expand_envp(test_str3);
+	char	*expanded_value4 = expand_envp(test_str4);
+
+	debug("test_str1 : %s, value1 : %s", test_str1, expanded_value1);
+	debug("test_str2 : %s, value2 : %s", test_str2, expanded_value2);
+	debug("test_str3 : %s, value3 : %s", test_str3, expanded_value3);
+	debug("test_str4 : %s, value4 : %s", test_str4, expanded_value4);
+
+	free(expanded_value1);
+	free(expanded_value2);
+	free(expanded_value3);
+	free(expanded_value4);
 }
 
-void	expand_envp_fail1(void)
+void	expand_envp_success_test2(void)
 {
-	const char	*path_var = ft_strjoin("SHLVL:", getenv("SHLVL"));
-	t_token		*token;
+	char	*test_str1 = "abc$USERdef";
+	char	*test_str2 = "abc\'$USER\'def";
+	char	*test_str3 = "abc\"$USER\"def";
 
-	token = create_empty_token();
-	token->type = TK_WORD;
-	token->value = ft_strdup("SHLVL:\\$SHLVL");
-	token = expand_envp(token);
-	if (ft_strncmp(token->value, path_var, ft_strlen(token->value)) != 0)
-		printf("[SUCCESS] before value : \"%s\", after value : \"%s\"\n",
-			"SHLVL:\\$SHLVL", (char *)(token->value));
-	free(token->value);
-	free(token);
-	free((void *)path_var);
-}
+	char	*expanded_value1 = expand_envp(test_str1);
+	char	*expanded_value2 = expand_envp(test_str2);
+	char	*expanded_value3 = expand_envp(test_str3);
 
-void	expand_envp_success1(void)
-{
-	const char	*path_var = ft_strjoin("SHLVL:", getenv("SHLVL"));
-	t_token		*token;
+	debug("test_str1 : %s, value1 : %s", test_str1, expanded_value1);
+	debug("test_str2 : %s, value2 : %s", test_str2, expanded_value2);
+	debug("test_str3 : %s, value3 : %s", test_str3, expanded_value3);
 
-	token = create_empty_token();
-	token->type = TK_WORD;
-	token->value = ft_strdup("SHLVL:$SHLVL");
-	token = expand_envp(token);
-	if (ft_strncmp(token->value, path_var, ft_strlen(token->value)) == 0)
-		printf("[SUCCESS] before value : \"%s\", after value : \"%s\"\n",
-			"SHLVL:$SHLVL", (char *)(token->value));
-	else
-		printf("[FAIL] before value : \"%s\", after value : \"%s\"\n",
-			"SHLVL:$SHLVL", (char *)(token->value));
-	free(token->value);
-	free(token);
-	free((void *)path_var);
-}
-
-void	expand_envp_success2(void)
-{
-	const char	*path_var = getenv("PATH");
-	t_token		*token;
-
-	token = create_empty_token();
-	token->type = TK_WORD;
-	token->value = ft_strdup("$PATH");
-	token = expand_envp(token);
-	if (ft_strncmp(token->value, path_var, ft_strlen(token->value)) == 0)
-		printf("[SUCCESS] before value : \"%s\", after value : \"%s\"\n",
-			"$PATH", (char *)(token->value));
-	else
-		printf("[FAIL] before value : \"%s\", after value : \"%s\"\n",
-			"$PATH", (char *)(token->value));
-	free(token->value);
-	free(token);
+	free(expanded_value1);
+	free(expanded_value2);
+	free(expanded_value3);
 }
 
 int	main(int argc, char *argv[], char *envp[])
@@ -102,13 +63,9 @@ int	main(int argc, char *argv[], char *envp[])
 	(void)argc;
 	(void)argv;
 	init_shell_data(envp);
-	expand_envp_success1();
-	printf("\n\n");
-	expand_envp_success2();
-	printf("\n\n");
-	expand_envp_fail1();
-	printf("\n\n");
-	expand_envp_dot_test();
-	clear_hashmap(&(get_shell_data()->envp_map));
+	expand_envp_success_test1();
+	printf("\n");
+	expand_envp_success_test2();
+	clear_shell_data();
 	exit(EXIT_SUCCESS);
 }
