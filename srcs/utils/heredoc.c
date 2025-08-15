1219e42f8b7f0df9b146c6f96394bd6544216c3a
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-15 23:40:25 +0900

    refactor: update heredoc function to comply with Norminette rules

--- srcs/utils/heredoc.c
+++ srcs/utils/heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
+/*   By: jyoo < jyoo@student.42gyeongsan.kr >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 20:13:41 by yoshin            #+#    #+#             */
-/*   Updated: 2025/08/08 19:43:03 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/13 11:41:58 by jyoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,39 +21,56 @@
 
 #include "libft.h"
 
+static void	read_input(char **input, const char *limiter, int tty_fd);
+static int	set_heredoc_fd(void);
+
 char	*heredoc(const char *limiter)
 {
 	int		tty_fd;
 	char	*input;
-	char	*line;
-	char	*temp;
 
-	tty_fd = open("/dev/tty", O_WRONLY);
-	if (tty_fd == -1)
-	{
-	    perror("open /dev/tty");
-		return (NULL);
-	}
 	input = NULL;
+	tty_fd = set_heredoc_fd();
+	read_input(&input, limiter, tty_fd);
+	close(tty_fd);
+	return (input);
+}
+
+static void	read_input(char **input, const char *limiter, int tty_fd)
+{
+	char	*temp;
+	char	*line;
+
 	while (TRUE)
 	{
 		write(tty_fd, "heredoc> ", 9);
 		line = readline("");
 		if (ft_strncmp(line, limiter, ft_strlen(line) + 1) == 0)
 			break ;
-		temp = input;
+		temp = *input;
 		if (temp)
 		{
-			input = ft_strjoin(temp, line);
+			*input = ft_strjoin(temp, line);
 			free(temp);
 		}
 		else
-			input = ft_strdup(line);
+			*input = ft_strdup(line);
 		free(line);
-		temp = input;
-		input = ft_strjoin(temp, "\n");
+		temp = *input;
+		*input = ft_strjoin(temp, "\n");
 		free(temp);
 	}
-	close(tty_fd);
-	return (input);
+}
+
+static int	set_heredoc_fd(void)
+{
+	int	tty_fd;
+
+	tty_fd = open("/dev/tty", O_WRONLY);
+	if (tty_fd == -1)
+	{
+		perror("open /dev/tty");
+		exit(EXIT_FAILURE);
+	}
+	return (tty_fd);
 }
