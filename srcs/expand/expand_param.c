d82ff6f028e0b130910116482bbd6eb0acdb2896
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-25 16:51:28 +0900

    refactor(expand_param.c): modify code style to comply with Norminette

--- srcs/expand/expand_param.c
+++ srcs/expand/expand_param.c
@@ -6,7 +6,7 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 16:29:55 by yoshin            #+#    #+#             */
-/*   Updated: 2025/08/25 15:29:10 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/25 16:51:04 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@
 static void	get_envpair(t_exp_token *exp_token, size_t idx, char ***env_pair);
 static char	*extract_envparam(char *str);
 static char	*get_envvalue(const char *env_param);
+static char	*get_special_value(char c);
 
 t_exp_token	*expand_param(t_exp_token *exp_token)
 {
@@ -34,7 +35,10 @@ t_exp_token	*expand_param(t_exp_token *exp_token)
 	get_envpair(exp_token, 0, &env_pair);
 	while (env_pair)
 	{
-		idx = find_next_delim_pos((exp_token->value), is_dollar_sign, C_BACKSLASH | C_SQUOTE);
+		idx = find_next_delim_pos(
+				(exp_token->value),
+				is_dollar_sign,
+				C_BACKSLASH | C_SQUOTE);
 		replace_exp(exp_token, &idx, env_pair);
 		free(env_pair[0]);
 		free(env_pair[1]);
@@ -53,14 +57,17 @@ static void	get_envpair(t_exp_token *exp_token, size_t idx, char ***env_pair)
 		return ;
 	(*env_pair)[PARAM_IDX] = NULL;
 	(*env_pair)[VALUE_IDX] = NULL;
-	dollar_pos = find_next_delim_pos(&((exp_token->value)[idx]), is_dollar_sign, C_BACKSLASH | C_SQUOTE);
+	dollar_pos = \
+		find_next_delim_pos(&((exp_token->value)[idx]), \
+			is_dollar_sign, C_BACKSLASH | C_SQUOTE);
 	if ((idx + dollar_pos) == ft_strlen(exp_token->value))
 	{
 		free(*env_pair);
 		*env_pair = NULL;
 		return ;
 	}
-	(*env_pair)[PARAM_IDX] = extract_envparam(&((exp_token->value)[idx + dollar_pos]));
+	(*env_pair)[PARAM_IDX] = \
+		extract_envparam(&((exp_token->value)[idx + dollar_pos]));
 	if (!(*env_pair)[PARAM_IDX])
 	{
 		free(*env_pair);
@@ -93,41 +100,41 @@ static char	*get_envvalue(const char *env_param)
 {
 	t_hash_map	*map;
 	char		*env_value;
-	const char	*cur;
 
 	if (!env_param)
 		return (NULL);
-	cur = env_param + 1;
-	env_value = NULL;
-	if (*cur == '?')
-		env_value = (ft_itoa(get_shell_data()->last_status));
-	else if (*cur == '$')
-		env_value = (ft_itoa(getpid()));
-	else if (*cur == '!')
+	env_value = get_special_value(*(env_param + 1));
+	if (!env_value)
 	{
-		if (get_shell_data()->last_bg_pid == -1)
-			env_value = ft_strdup("");
-		else
-			env_value = ft_itoa(get_shell_data()->last_bg_pid);
-	}
-	else if (ft_isdigit(*cur))
-	{
-		if (*cur == '0')
-			env_value = ft_strdup("minishell");
-		else
-			env_value = ft_strdup("");
-	}
-	else if ((*cur == '_' && *(cur + 1) == '\0')
-		|| *cur == '*' || *cur == '@')
-	{
-		env_value = ft_strdup("");
-	}
-	else
-	{
-		map = &((get_shell_data())->envp_map);
+		map = &(get_shell_data()->envp_map);
 		env_value = ft_strdup(get_value(map, env_param + 1));
 	}
 	if (!env_value)
 		return (ft_strdup(""));
 	return (env_value);
+}
+
+static char	*get_special_value(char c)
+{
+	if (c == '?')
+		return (ft_itoa(get_shell_data()->last_status));
+	if (c == '$')
+		return (ft_itoa(getpid()));
+	if (c == '!')
+	{
+		if ((get_shell_data())->last_bg_pid == -1)
+			return (ft_strdup(""));
+		else
+			return (ft_itoa(get_shell_data()->last_bg_pid));
+	}
+	if (ft_isdigit(c))
+	{
+		if (c == '0')
+			return (ft_strdup("minishell"));
+		else
+			return (ft_strdup(""));
+	}
+	if (c == '_' || c == '*' || c == '@')
+		return (ft_strdup(""));
+	return (NULL);
 }
