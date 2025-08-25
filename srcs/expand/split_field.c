183d4056140186095fe40f8165eb6152f82dd01c
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-25 16:36:37 +0900

    refactor(split_field.c): modify code style to comply with Norminette

--- srcs/expand/split_field.c
+++ srcs/expand/split_field.c
@@ -6,70 +6,87 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 16:56:12 by yoshin            #+#    #+#             */
-/*   Updated: 2025/08/25 15:39:13 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/25 16:33:54 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
 #include "libft.h"
-
 #include "def.h"
 #include "utils.h"
 #include "expand.h"
 
+static void		update_state(t_boolean state[3], char c);
+
+static void		add_token(
+					t_list **exp_list,
+					t_exp_token *exp_token,
+					size_t *start,
+					size_t *end);
+
+static size_t	skip_ifs(const char *str, size_t idx);
+
 void	split_field(t_list **exp_list, t_exp_token *exp_token)
 {
-	t_exp_token	*new_exp_token;
-	size_t			idx;
-	size_t			last_idx;
 	t_boolean	state[3];
+	size_t		end;
+	size_t		start;
 
 	if (!exp_list || !exp_token)
 		return ;
 	state[IN_SQUOTE] = FALSE;
 	state[IN_DQUOTE] = FALSE;
 	state[IN_ESCAPE] = FALSE;
-	new_exp_token = NULL;
-	idx = 0;
-	while (is_ifs((exp_token->value)[idx]))
-		idx++;
-	last_idx = idx;
-	while ((exp_token->value)[idx])
+	end = skip_ifs(exp_token->value, 0);
+	start = end;
+	while (exp_token->value[end])
 	{
-		if (state[IN_ESCAPE])
-			state[IN_ESCAPE] = FALSE;
-		else if ((exp_token->value)[idx] == '\\' && !state[IN_SQUOTE])
-			state[IN_ESCAPE] = TRUE;
-		else if ((exp_token->value)[idx] == '\'' && !state[IN_DQUOTE])
-			state[IN_SQUOTE] = !state[IN_SQUOTE];
-		else if ((exp_token->value)[idx] == '"' && !state[IN_SQUOTE])
-			state[IN_DQUOTE] = !state[IN_DQUOTE];
+		update_state(state, exp_token->value[end]);
+		if (!state[IN_ESCAPE] && !state[IN_SQUOTE] && !state[IN_DQUOTE]
+			&& is_ifs(exp_token->value[end]))
+			add_token(exp_list, exp_token, &start, &end);
 		else
-		{
-			if ((!state[IN_ESCAPE] && !state[IN_SQUOTE] && !state[IN_DQUOTE])
-				&& is_ifs((exp_token->value)[idx]))
-			{
-				new_exp_token = create_empty_exp_token();
-				new_exp_token->value = ft_substr(&(exp_token->value)[last_idx], 0, idx - last_idx);
-				new_exp_token->qmask = ft_substr(&(exp_token->qmask)[last_idx], 0, idx - last_idx);
-				ft_lstadd_back(exp_list, ft_lstnew(new_exp_token));
-				while (is_ifs(((exp_token)->value)[idx]))
-					idx++;
-				last_idx = idx;
-				continue ;
-			}
-		}
-		idx++;
+			end++;
 	}
-	if (last_idx != ft_strlen(exp_token->value))
-	{
-		new_exp_token = create_empty_exp_token();
-		new_exp_token->value = ft_substr(&(exp_token->value)[last_idx], 0, idx - last_idx);
-		new_exp_token->qmask = ft_substr(&(exp_token->qmask)[last_idx], 0, idx - last_idx);
-		ft_lstadd_back(exp_list, ft_lstnew(new_exp_token));
-	}
+	if (start != end)
+		add_token(exp_list, exp_token, &start, &end);
 	free(exp_token->value);
 	free(exp_token->qmask);
 	free(exp_token);
+}
+
+static void	update_state(t_boolean state[3], char c)
+{
+	if (state[IN_ESCAPE])
+		state[IN_ESCAPE] = FALSE;
+	else if (c == '\\' && !state[IN_SQUOTE])
+		state[IN_ESCAPE] = TRUE;
+	else if (c == '\'' && !state[IN_DQUOTE])
+		state[IN_SQUOTE] = !state[IN_SQUOTE];
+	else if (c == '"' && !state[IN_SQUOTE])
+		state[IN_DQUOTE] = !state[IN_DQUOTE];
+}
+
+static void	add_token(
+				t_list **exp_list,
+				t_exp_token *exp_token,
+				size_t *start,
+				size_t *end)
+{
+	t_exp_token	*new;
+
+	new = create_empty_exp_token();
+	new->value = ft_substr(&(exp_token->value)[*start], 0, *end - *start);
+	new->qmask = ft_substr(&(exp_token->qmask)[*start], 0, *end - *start);
+	ft_lstadd_back(exp_list, ft_lstnew(new));
+	*end = skip_ifs(exp_token->value, *end);
+	*start = *end;
+}
+
+static size_t	skip_ifs(const char *str, size_t idx)
+{
+	while (is_ifs(str[idx]))
+		idx++;
+	return (idx);
 }
