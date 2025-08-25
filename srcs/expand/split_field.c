76cd47faea4f355705f53968bb2f6db1c18b3f05
yoshin <yoshin@student.42gyeongsan.kr>
2025-08-25 15:56:00 +0900

    fix(split_field): trim IFS characters from expanded token value

--- srcs/expand/split_field.c
+++ srcs/expand/split_field.c
@@ -6,7 +6,7 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 16:56:12 by yoshin            #+#    #+#             */
-/*   Updated: 2025/08/25 15:26:13 by yoshin           ###   ########.fr       */
+/*   Updated: 2025/08/25 15:39:13 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@
 void	split_field(t_list **exp_list, t_exp_token *exp_token)
 {
 	t_exp_token	*new_exp_token;
-	int			idx;
-	int			last_idx;
+	size_t			idx;
+	size_t			last_idx;
 	t_boolean	state[3];
 
 	if (!exp_list || !exp_token)
@@ -30,9 +30,11 @@ void	split_field(t_list **exp_list, t_exp_token *exp_token)
 	state[IN_SQUOTE] = FALSE;
 	state[IN_DQUOTE] = FALSE;
 	state[IN_ESCAPE] = FALSE;
-	idx = 0;
-	last_idx = 0;
 	new_exp_token = NULL;
+	idx = 0;
+	while (is_ifs((exp_token->value)[idx]))
+		idx++;
+	last_idx = idx;
 	while ((exp_token->value)[idx])
 	{
 		if (state[IN_ESCAPE])
@@ -60,10 +62,13 @@ void	split_field(t_list **exp_list, t_exp_token *exp_token)
 		}
 		idx++;
 	}
-	new_exp_token = create_empty_exp_token();
-	new_exp_token->value = ft_substr(&(exp_token->value)[last_idx], 0, idx - last_idx);
-	new_exp_token->qmask = ft_substr(&(exp_token->qmask)[last_idx], 0, idx - last_idx);
-	ft_lstadd_back(exp_list, ft_lstnew(new_exp_token));
+	if (last_idx != ft_strlen(exp_token->value))
+	{
+		new_exp_token = create_empty_exp_token();
+		new_exp_token->value = ft_substr(&(exp_token->value)[last_idx], 0, idx - last_idx);
+		new_exp_token->qmask = ft_substr(&(exp_token->qmask)[last_idx], 0, idx - last_idx);
+		ft_lstadd_back(exp_list, ft_lstnew(new_exp_token));
+	}
 	free(exp_token->value);
 	free(exp_token->qmask);
 	free(exp_token);
